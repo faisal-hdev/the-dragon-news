09a720b44a92568ec204eae507dd96f96f9f24dc
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-04-03 23:28:03 +0600

    Full project created and setup or install done by vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-8xl font-bold">Hello BD hello faisal</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNR4B5CONHS79MUqXQu4odJ11ZMj8Bg8s",
+  authDomain: "the-dragon-news-16147.firebaseapp.com",
+  projectId: "the-dragon-news-16147",
+  storageBucket: "the-dragon-news-16147.appspot.com",
+  messagingSenderId: "70159415307",
+  appId: "1:70159415307:web:db5d25d69ff42bbb7aef0e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
